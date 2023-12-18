b4b2a2c2fefca9f4156a39af588369ccc4904aab
Albirozado <albirosaldoalfredo12@gmail.com>
2023-12-18 15:46:33 -0800

    Responsive

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,20 +1,23 @@
+import { useState } from "react";
 import NavBar from "./FrontEnd/NavBar";
 import Sidebar from "./FrontEnd/Sidebar";
 import Chat from "./FrontEnd/chat";
 
 export default function DefaultLayout(){
+    const [openSide, setOpenSIde] = useState(false)
 
     return(
         <section className="h-screen max-w-full w-[1450px] bg-gray-200 mx-auto overflow-y-hidden">
-            <header className=" bg-[#f59300] py-3 px-8">
-                <NavBar/>
+            <header className=" bg-[#f59300]">
+                <NavBar openSide = {openSide} setOpenSIde={setOpenSIde}/>
             </header>
                 <div className="h-full flex w-full max-w-full">
-                    <div className=" bg-white w-[38%] lg:w-[30%] h-full overflow-y-scroll sideBar pb-28">
-                           <Sidebar/>
+                <div className={openSide ? " z-50 max-w-full left-[0rem] bg-white w-[25rem] md:w-[38%] md:left-[0] lg:w-[30%] h-full overflow-y-scroll sideBar pb-20  absolute md:relative transition-all duration-300 ease-in-out" : "max-w-full left-[-25rem] bg-white w-[25rem] md:w-[38%] md:left-[0] lg:w-[30%] h-full overflow-y-scroll sideBar pb-28 absolute md:relative transition-all duration-300 ease-in-out"}>
+                      <Sidebar/>
+                      
                     </div>
                     
-                    <div className="bg- bg-[#fffaf6] w-[62%] lg:w-[70%] max-w-full ">
+                    <div className="bg-[#fffaf6] w-[100%] md:w-[62%] lg:w-[70%]  max-w-full" >
                         <Chat/>
                     </div>
                 </div>
